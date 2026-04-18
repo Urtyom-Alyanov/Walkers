b4319c0b540cf64ad17084337cd69eeefa65c9f6
Urtyom-Alyanob <mc2017artemklochkcov435@yandex.ru>
2026-04-18 10:46:14 +0300

    init: add core and forms projects

---
+++ source/Walkers.Core/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace Walkers.Core;
+
+public class Class1
+{
+
+}
---
+++ source/Walkers.FormsApp/Form1.cs
@@ -0,0 +1,9 @@
+namespace Walkers.FormsApp;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+}
